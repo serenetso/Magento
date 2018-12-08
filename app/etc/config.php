85e1029575aa947a3dcc5b2f5db5411ed098fe38
saranguatsogtoo <sarangua97@gmail.com>
2018-12-08 19:48:55 +0100

    Config changes

--- app/etc/config.php
+++ app/etc/config.php
@@ -194,5 +194,34 @@ return [
         'Mageplaza_Gdpr' => 1,
         'Temando_Shipping' => 1,
         'Vertex_Tax' => 1
+    ],
+    'themes' => [
+        'frontend/Magento/blank' => [
+            'parent_id' => NULL,
+            'theme_path' => 'Magento/blank',
+            'theme_title' => 'Magento Blank',
+            'is_featured' => '0',
+            'area' => 'frontend',
+            'type' => '0',
+            'code' => 'Magento/blank'
+        ],
+        'adminhtml/Magento/backend' => [
+            'parent_id' => NULL,
+            'theme_path' => 'Magento/backend',
+            'theme_title' => 'Magento 2 backend',
+            'is_featured' => '0',
+            'area' => 'adminhtml',
+            'type' => '0',
+            'code' => 'Magento/backend'
+        ],
+        'frontend/Magento/luma' => [
+            'parent_id' => 'Magento/blank',
+            'theme_path' => 'Magento/luma',
+            'theme_title' => 'Magento Luma',
+            'is_featured' => '0',
+            'area' => 'frontend',
+            'type' => '0',
+            'code' => 'Magento/luma'
+        ]
     ]
 ];
